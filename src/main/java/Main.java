dbca4cc63b4ac77edceb0a9e5449b66907cbdb86
Nishtha Chaudhary <nishthakc@gmail.com>
2024-12-24 01:13:21 -0700

    PWD and CD commands implemented!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,47 +15,73 @@ public class Main {
         return null;
     } 
     public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
         System.out.print("$ ");
 
-        Scanner scanner = new Scanner(System.in);
-        String input;
-        Set<String> builtIn = Set.of("echo","exit","type");
-        while((input = scanner.nextLine())!=null){
-            if(input.contains("exit 0")){
-                break;
-            }
-            else if(input.startsWith("echo")){
-                System.err.println(input.substring(5));
-            }
-            else if(input.startsWith("type")){
-                String param = input.substring(5);
-                if(builtIn.contains(param)){
-                    System.err.println(param + " is a shell builtin");
+        try (Scanner scanner = new Scanner(System.in)) {
+            String input;
+            Set<String> builtIn = Set.of("echo","exit","type","pwd","cd");
+            String currDir = System.getProperty("user.dir");
+
+            while((input = scanner.nextLine())!=null){
+                String cmd = input.split(" ")[0];
+                String param = "";
+                if(input.length()>cmd.length()){
+                    param = input.substring(cmd.length()+1);
                 }
-                else{
-                    String path = getPath(param);
-                    if(path != null){
-                        System.err.println(param + " is " + path);
+
+                if(cmd.equals("exit") && param.equals("0")){
+                    break;
+                }
+                else if(cmd.equals("echo")){
+                    System.out.println(param);
+                }
+                else if(cmd.equals("type")){
+                    if(builtIn.contains(param)){
+                        System.out.println(param + " is a shell builtin");
                     }
                     else{
-                        System.out.println(param + ": not found");
+                        String path = getPath(param);
+                        if(path != null){
+                            System.out.println(param + " is " + path);
+                        }
+                        else{
+                            System.out.println(param + ": not found");
+                        }
                     }
                 }
-            }
-            else{
-                String cmd = input.split(" ")[0];
-                String path = getPath(cmd);
-                if(path != null){
-                    String fullPath = path + input.substring(cmd.length());
-                    Process p = Runtime.getRuntime().exec(fullPath.split(" "));
-                    p.getInputStream().transferTo(System.out);
+                else if(cmd.equals("pwd")){
+                    System.out.println(currDir);
+                }
+                else if(cmd.equals("cd")){
+                    if(param.equals("~")){
+                        currDir = System.getenv("HOME");
+                    }
+                    else{
+                        if(!param.startsWith("/")){
+                            param = currDir + "/" + param;
+                        }
+    
+                        if(Files.isDirectory(Path.of(param))){
+                            currDir = Path.of(param).normalize().toString();
+                        }
+                        else{
+                            System.out.println("cd: " + param + ": No such file or directory");
+                        }
+                    }
                 }
                 else{
-                    System.out.println(input + ": command not found");
+                    String path = getPath(cmd);
+                    if(path != null){
+                        String fullPath = path + input.substring(cmd.length());
+                        Process p = Runtime.getRuntime().exec(fullPath.split(" "));
+                        p.getInputStream().transferTo(System.out);
+                    }
+                    else{
+                        System.out.println(input + ": command not found");
+                    }
                 }
+                System.out.print("$ ");
             }
-            System.out.print("$ ");
         }
     }
 }
